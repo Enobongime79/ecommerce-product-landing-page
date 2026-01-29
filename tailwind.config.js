7166bf77402378b320b6800602a2941b2ffc5b69
Enobongime79 <enobongime79@gmail.com>
2026-01-29 16:53:33 +0100

    already done with the navbar

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html", "./src/**/*.js"], // update to match your folders
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
