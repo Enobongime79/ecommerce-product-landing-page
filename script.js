1d16264c690a740abfffc47f9676486594329dc1
Enobongime79 <enobongime79@gmail.com>
2026-01-30 23:14:30 +0100

    Finally done with the responsive design for all screen sizes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-  const carousel = document.getElementById("carousel");
-  const slides = carousel.children;
-  let index = 0;
+const carousel = document.getElementById("carousel");
+const slides = carousel.children;
+let index = 0;
 
-  function updateCarousel() {
+function updateCarousel() {
     carousel.style.transform = `translateX(-${index * 100}%)`;
   }
 
@@ -15,3 +15,29 @@
     index = (index - 1 + slides.length) % slides.length;
     updateCarousel();
   }
+
+const orderAmt = document.getElementById("orderAmt");
+const plusBtn = document.getElementById("plusBtn");
+const minusBtn = document.getElementById("minusBtn");
+
+const cart = document.getElementById("cart");
+
+plusBtn.addEventListener("click", () => {
+  let value = Number(orderAmt.textContent);
+  value++;
+  orderAmt.textContent = value;
+})
+
+
+minusBtn.addEventListener("click", () => {
+  let value = Number(orderAmt.textContent);
+
+  if (value > 0){
+    value--;
+    orderAmt.textContent = value;
+  }
+})
+
+cart.addEventListener("click", () => {
+  cart.setAttribute("shadow", "none")
+})
