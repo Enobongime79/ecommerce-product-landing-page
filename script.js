784c8fdd6fcbbb21ab41261a20b0e983455d1a3c
Enobongime79 <enobongime79@gmail.com>
2026-01-31 14:14:54 +0100

    updated and finshed the carousel of the desktop view

--- script.js
+++ script.js
@@ -41,3 +41,67 @@ minusBtn.addEventListener("click", () => {
 cart.addEventListener("click", () => {
   cart.setAttribute("shadow", "none")
 })
+
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("closeSidebar");
+
+function openSidebar() {
+    sidebar.classList.remove("-translate-x-full");
+    overlay.classList.remove("hidden");
+}
+
+function closeSidebar() {
+    sidebar.classList.add("-translate-x-full");
+    overlay.classList.add("hidden");
+}
+
+hamburger.addEventListener("click", openSidebar);
+closeBtn.addEventListener("click", closeSidebar);
+overlay.addEventListener("click", closeSidebar);
+
+const thumbnailOne = document.getElementById("thumbnailOne");
+const thumbnailTwo = document.getElementById("thumbnailTwo");
+const thumbnailThree = document.getElementById("thumbnailThree");
+const thumbnailFour = document.getElementById("thumbnailFour");
+
+thumbnailOne.addEventListener("click", () => {
+  index = 0;
+  updateCarousel();
+  thumbnailOne.setAttribute("class", "cursor-pointer h-19 rounded-lg opacity-60 border-amber-500 border-2")
+  thumbnailTwo.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailThree.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailFour.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+})
+
+thumbnailTwo.addEventListener("click", () => {
+  index = 1 % slides.length;
+  updateCarousel();
+  thumbnailTwo.setAttribute("class", "cursor-pointer h-19 rounded-lg opacity-60 border-amber-500 border-2")
+  thumbnailOne.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailThree.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailFour.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+})
+
+thumbnailThree.addEventListener("click", () => {
+  index = 2 % slides.length;
+  updateCarousel();
+  thumbnailThree.setAttribute("class", "cursor-pointer h-19 rounded-lg opacity-60 border-amber-500 border-2")
+  thumbnailTwo.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailOne.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailFour.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+})
+
+thumbnailFour.addEventListener("click", () => {
+  index = 3 % slides.length;
+  updateCarousel();
+  thumbnailFour.setAttribute("class", "cursor-pointer h-19 rounded-lg opacity-60 border-amber-500 border-2")
+  thumbnailTwo.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailThree.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+  thumbnailOne.setAttribute("class", "cursor-pointer h-19 rounded-lg hover:opacity-60")
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  thumbnailOne.setAttribute("class", "cursor-pointer h-19 rounded-lg opacity-60 border-amber-500 border-2")
+})
